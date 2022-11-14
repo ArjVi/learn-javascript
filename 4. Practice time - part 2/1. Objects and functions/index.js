23a69c0d38d160b719d075fd27f2dd81cce13cd3
Arjun Singh <117516065+ArjVi@users.noreply.github.com>
2022-11-14 21:19:47 +0100

    yes

--- 4. Practice time - part 2/1. Objects and functions/index.js
+++ 4. Practice time - part 2/1. Objects and functions/index.js
@@ -1,8 +1,22 @@
 // Create a person object that contains three keys: name, age, and county.
 // Use yourself as an example to set the values for name, age, and country
 
+let player = {
+    name: "Arjun",
+    age: 17,
+    country: "Poland"
+}
+
 // Create a function, logData(), that uses the person object to create a
 // string in the following format: 
 // "Per is 35 years old and lives in Norway"
 
+function logData() {
+    let str = player.name + " is " + player.age + " years old and lives in " + player.country
+    
+    console.log(str)
+}
+
 // Call the logData() function to verify that it works
+
+logData()
